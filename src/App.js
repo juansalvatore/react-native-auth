e922a541bb324cdbcb1dd10804c9c59079896c0e
Juan Salvatore <juansalvatore@live.com.ar>
2018-01-03 12:41:45 -0300

    Add styles to log out button and create onClick function for logging out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase'
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Header } from './components/common'
+import { Button, Header, CardSection, Spinner } from './components/common'
 import LoginForm from './components/LoginForm'
 
 export default class App extends Component {
+  state = {
+    loggedIn: null,
+  }
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyC8DBkuBZ7CY_Fh6fSe03eVbVvfbskHag8',
@@ -14,14 +17,59 @@ export default class App extends Component {
       storageBucket: 'auth-5ea88.appspot.com',
       messagingSenderId: '194751110876',
     })
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent = () => {
+    const { logOutStyle, spinnerStyle } = styles
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={logOutStyle}>
+            <CardSection>
+              <Button
+                onPress={() => {
+                  firebase.auth().signOut()
+                }}
+              >
+                Log out
+              </Button>
+            </CardSection>
+          </View>
+        )
+      case false:
+        return <LoginForm />
+      default:
+        return (
+          <View style={spinnerStyle}>
+            <Spinner size="large" />
+          </View>
+        )
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Auth" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
+}
+
+const styles = {
+  logOutStyle: {
+    top: 25,
+  },
+  spinnerStyle: {
+    top: 350,
+  },
 }
