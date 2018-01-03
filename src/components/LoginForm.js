e5481a9e19335789ccee3d01e70e055af1c390d1
Juan Salvatore <juansalvatore@live.com.ar>
2018-01-03 11:41:48 -0300

    Create spinner for login and register

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,47 @@
+import firebase from 'firebase'
 import React, { Component } from 'react'
-import { Button, Card, CardSection, Input } from './common'
+import { Text } from 'react-native'
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
+    error: '',
+    loading: false,
+  }
+  onButtonPress = () => {
+    const { email, password } = this.state
+    this.setState({ error: '', loading: true })
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(error => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this))
+      })
   }
 
+  onLoginFail = error => {
+    this.setState({
+      error: error.message,
+      loading: false,
+    })
+  }
+
+  onLoginSuccess = () => {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: '',
+    })
+  }
   render() {
     return (
       <Card>
@@ -27,12 +62,25 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
         <CardSection>
-          <Button>Log in</Button>
+          {this.state.loading ? (
+            <Spinner size="small" />
+          ) : (
+            <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
+          )}
         </CardSection>
       </Card>
     )
   }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
 }
 
 export default LoginForm
